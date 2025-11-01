098f69f796953b4b9d7351358f5820d77c1b3197
celiabustea <150381439+celiabustea@users.noreply.github.com>
2025-11-01 17:57:53 +0200

    Delete backend directory

--- backend/src/routes/eventRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import eventController from '@controllers/eventController';
-
-const router = Router();
-router.get('/', eventController.getUsers);
-
-export default router;
